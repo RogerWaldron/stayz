6001e430085877910ac648fb89bae0a9fc874846
rwaldron <outsideinprofits@gmail.com>
2023-08-09 22:13:35 +1000

    added Hotel model

---
+++ Stayz.Domain/Models/Hotel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Stayz.Domain.Models
+{
+	public class Hotel
+	{
+		public int HotelId { get; set; }
+		public required string Name { get; set; }
+		public required string Address { get; set; }
+		public required string City { get; set; }
+        public required string Country { get; set; }
+		public int Stars { get; set; }
+        public required List<Room> Rooms { get; set; }
+	}
+}
+
