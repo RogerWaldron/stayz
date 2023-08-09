b5f9980e0ad2b81ced26847087060232f1473d6f
rwaldron <outsideinprofits@gmail.com>
2023-08-09 22:27:57 +1000

    added HotelValidation

---
+++ Stayz.Domain/Validation/HotelValidation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Stayz.Domain.Models;
+
+namespace Stayz.Domain.Validation
+{
+	public class HotelValidation
+	{
+		public bool IsHotelValid(Hotel hotel)
+		{
+			return true;
+		}
+	}
+}
+
