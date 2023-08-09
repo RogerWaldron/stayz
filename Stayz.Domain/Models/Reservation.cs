9984984b131dad2ca13f1634376733fd76727274
rwaldron <outsideinprofits@gmail.com>
2023-08-09 22:24:11 +1000

    refactor: Room, StartDate and EndDate all Nullable

--- Stayz.Domain/Models/Reservation.cs
+++ Stayz.Domain/Models/Reservation.cs
@@ -6,8 +6,8 @@ namespace Stayz.Domain.Models
 		public int ReservationId { get; set; }
 		public required string Customer { get; set; }
         public Room? Room { get; set; }
-		public DateTime StartDate { get; set; }
-		public DateTime EndDate { get; set; }
+		public DateTime? StartDate { get; set; }
+		public DateTime? EndDate { get; set; }
 	}
 }
 
