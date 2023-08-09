a6c95392f7f747e22351fc580ff70cefbe385ade
rwaldron <outsideinprofits@gmail.com>
2023-08-09 22:31:11 +1000

    add Hotel constructor

--- Stayz.Domain/Models/Hotel.cs
+++ Stayz.Domain/Models/Hotel.cs
@@ -3,6 +3,12 @@ namespace Stayz.Domain.Models
 {
 	public class Hotel
 	{
+		public Hotel(string name, int stars, string address)
+		{
+			if (string.IsNullOrWhiteSpace(name))
+				throw new ArgumentException("Hotel name is not allowed to be null or whitespace");
+		}
+
 		public int HotelId { get; set; }
 		public required string Name { get; set; }
 		public required string Address { get; set; }
