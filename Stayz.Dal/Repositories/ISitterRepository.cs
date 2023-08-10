88bcb3f813d0c42e43f26671237d8f7c81151759
rwaldron <outsideinprofits@gmail.com>
2023-08-10 22:39:45 +1000

    feat: created Interface and Class for Sitter repo

---
+++ Stayz.Dal/Repositories/ISitterRepository.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Stayz.Domain.Abstractions.Repositories;
+using Stayz.Domain.Models;
+
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Stayz.Dal.Repositories
+{
+	public class SitterRepository: ISitterRepository
+	{
+		private readonly DataContext _ctx;
+		private int _nextId = 1;
+
+		public SitterRepository(DataContext ctx)
+		{
+			_ctx = ctx;
+		}
+
+		public async Task<List<Sitter>> GetAllSittersAsync()
+		{
+			return await _ctx.Sitters.ToListAsync();
+		}
+
+		public async Task<Sitter> GetSitterByIdAsync(int id)
+		{
+			var sitter = await _ctx.Sitters
+				.Include(s => s.Stays)
+				.FirstOrDefault(s => s.Id == id);
+
+			if (sitter == null)
+				return null;
+
+			return sitter;
+		}
+
+        public async Task<Sitter> CreateSitterAsync(Sitter sitter)
+        {
+            _ctx.Sitters.Add(sitter);
+            await _ctx.SaveChangesAsync();
+
+            return sitter;
+        }
+
+        public async Task<Sitter> UpdateSitterAsync(Sitter updatedSitter)
+        {
+            _ctx.Sitters.Update(updatedSitter);
+            await _ctx.SaveChangesAsync();
+
+            return updatedSitter;
+        }
+
+        Task<Sitter> ISitterRepository.DeleteSitterAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<List<Stay>> ISitterRepository.ListSitterStaysAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<Stay> ISitterRepository.GetSitterStayByIdAsync(int sitterId, int stayId)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<Stay> ISitterRepository.CreateSitterStayAsync(int sitterId, Stay stay)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<Stay> ISitterRepository.UpdateSitterStayAsync(int sitterId, Stay updatedStay)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<Stay> ISitterRepository.DeleteSitterStayAsync(int sitterId, int stayId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
---
+++ Stayz.Domain/Abstractions/Repositories/SitterRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Stayz.Domain.Models;
+
+namespace Stayz.Domain.Abstractions.Repositories
+{
+	public interface ISitterRepository
+	{
+		Task<List<Sitter>> GetAllSittersAsync();
+		Task<Sitter> GetSitterByIdAsync(int id);
+		Task<Sitter> CreateSitterAsync(Sitter sitter);
+        Task<Sitter> UpdateSitterAsync(Sitter updatedSitter);
+        Task<Sitter> DeleteSitterAsync(int id);
+
+		Task<List<Stay>> ListSitterStaysAsync();
+		Task<Stay> GetSitterStayByIdAsync(int sitterId, int stayId);
+		Task<Stay> CreateSitterStayAsync(int sitterId, Stay stay);
+		Task<Stay> UpdateSitterStayAsync(int sitterId, Stay updatedStay);
+		Task<Stay> DeleteSitterStayAsync(int sitterId, int stayId);
+	}
+}
+
